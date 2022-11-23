a525771b2b035862495105764af2e87c1d547315
Sally Morris <105070505+softwaresally@users.noreply.github.com>
2022-11-22 20:26:38 -0500

    Changed App.js url to Render URL

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from 'react'
 const App = () => {
   const [name, setName] = useState([])
 
+  // useEffect(() => {
+  //   // fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
+  //   fetch(`http://localhost:5000/generate`)
+  //     .then((res) => res.json())
+  //     .then((nme) => setName([nme]))
+  // }, [])
+
   useEffect(() => {
-    // fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
-    fetch(`http://localhost:5000/generate`)
+    fetch('https://starter-name-generator.onrender.com/api/generate') // Change this URL to the one that you just deployed
       .then((res) => res.json())
-      .then((nme) => setName([nme]))
+      .then((name) => setName([name]))
   }, [])
 
   return (
